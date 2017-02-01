87a703de514bed4905968db7125ae9fb5e134653
Zoltan Biber <zoltan.biber@gmail.com>
2017-02-01 12:07:19 +0100

    Top video changes on click (on video list items).

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
 
-import App from './components/app';
-import reducers from './reducers';
+const YOUTUBE_API_KEY = 'AIzaSyAWLqCcfEXUOGjXpjciIQwxX9WcpSqd2aM';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state= {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({ key: YOUTUBE_API_KEY, term: 'surfboards'}, (videos) => {
+      // this.setState({ videos });  ES6 syntactic sugar for this.setState({ videos: videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})} // ES6, no need to write the function separately...
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
